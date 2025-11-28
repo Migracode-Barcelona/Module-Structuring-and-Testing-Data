c0e0f5aa5d29a4083f8ad001f2f2b88ec67a7525
Roman <roman.boxes@gmail.com>
2025-11-28 13:02:52 +0100

    Exercise 4
    - Worked with random function and which numbers are inclusive or exclusive

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,12 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+console.log(num);
+
+// Explanation:
+// 1. Math.random() generates a random floating-point number between 0 (inclusive) and 1 (exclusive) [0, 1) .
+// 2. Multiplying this random number by (maximum - minimum + 1) scales it to a range of 0 (inclusive) to (maximum - minimum + 1) (exclusive).
+// 3. Math.floor() then rounds this scaled number down to the nearest whole number, resulting in an integer from 0 to (maximum - minimum).
+// 4. Finally, adding 'minimum' shifts this range up, resulting in a final value between 'minimum' and 'maximum' (both inclusive).
+// Therefore, 'num' represents a random integer between 1 and 100, inclusive.
