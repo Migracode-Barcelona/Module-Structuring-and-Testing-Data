f16935480db082b6d4ef8d5fe0b884fcceb53339
Roman <roman.boxes@gmail.com>
2025-11-28 13:13:20 +0100

    2 Errors File 2 const & let
    - declare a variables correctly, let instead of const

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,6 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+console.log(age);
+// The code above works fine. It was an error because of using 'const' instead of 'let'
