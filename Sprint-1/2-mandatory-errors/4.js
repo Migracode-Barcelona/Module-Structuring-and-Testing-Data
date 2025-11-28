1c417c3eabcb37f4efdaec173684c4abbf0bca84
Roman <roman.boxes@gmail.com>
2025-11-28 14:29:21 +0100

    2 Errors File 3  Variable declaration
    Variable names cannot start with a number

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,8 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+
+// Variable declaration expected.ts(1134)
+// The error occurs because variable names cannot start with a number in JavaScript.
+// To fix the error, we can rename the variables to start with a letter or an underscore.
+const hourClockTime12 = "20:53";
+const hourClockTimeEight24 = "08:53";
+
+console.log(`12-hour clock time is ${hourClockTime12} and 24-hour clock time is ${hourClockTimeEight24}`);
