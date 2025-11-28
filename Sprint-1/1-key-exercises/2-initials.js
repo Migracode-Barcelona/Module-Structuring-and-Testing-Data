83df566fa79aee1b61fe8584ab9d9b9af93f544a
Roman <roman.boxes@gmail.com>
2025-11-28 11:46:34 +0100

    Exercise 2:
    Expected output: "CKJ"
    - used charAt method to get first character of the name
    - used with word as a array of símbolos

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -4,8 +4,17 @@ let lastName = "Johnson";
 
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
+const index = 0;
+let initials = firstName.charAt(index) +
+               middleName.charAt(index) +
+               lastName.charAt(index);
 
-let initials = ``;
+console.log(`The first letters of ${firstName}'s name are ${initials}`);
+// Expected output: "CKJ"
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
 
+
+
+console.log(firstName[0] + middleName[0] + lastName[0]);
+// Expected output: "CKJ
