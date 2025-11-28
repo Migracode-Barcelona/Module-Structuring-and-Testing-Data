7e77b0972f3ef44f54a61cc9e17aedeb942c62f5
Roman <roman.boxes@gmail.com>
2025-11-28 11:05:40 +0100

    Exercise 1
    Explained what the line 1 and the line 7 did. Added comments, and  consol.log to see the output in the terminal

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,8 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// Line 3 is updating the value of the count variable by adding 1 to its current value. 
+// The = operator assigns the new value back to the count variable.
+
+console.log(count);
+// Expected output: 1
