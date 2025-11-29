fbbdb152272a6ad3acb4159c660cf611a6affb46
Laura <laurabejaranoa22@gmail.com>
2025-11-29 15:52:25 +0100

    answer 4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,10 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+//const 12HourClockTime = "20:53";
+//const 24hourClockTime = "08:53";
+
+//Answer: Is invalid because variable names cannot start with a number.  So it will give a SyntaxError.
+
+const twuelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
+
+console.log(twuelveHourClockTime);
+console.log(twentyFourHourClockTime);   
