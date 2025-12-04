4fa8e19b6a5ba09925a22b1b526c46d750571ac6
marissamolejon <molejonmarissa@gmail.com>
2025-12-04 11:00:19 +0100

    testing

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,10 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+//(maximum - minimum +1) this will give a result of 99+1
+//Math.random will give a number lesser 1
+//Math.floor(Math.random)*(maximum - minimum +1) this expression will round down the value
+//* minimum will add 1 to the 99
+
+//num = will be a random value from 0 to 100
