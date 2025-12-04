8f22622516b4194d12d7424e793c53b12369ddc3
Ifeoma <osegboifeoma3@gmail.com>
2025-12-04 12:45:43 +0100

    describing and explaining what some attriutes do.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -1,6 +1,11 @@
 let count = 0;
 
 count = count + 1;
+console.log(count)
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+
+// line 3 is assigning a new value to the count variable
+// the operator = is used to set the value of the variable count to the expression count + 1.
