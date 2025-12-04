6b81ffe13c1c40c93ad4532769666c8b0f6b4948
Ifeoma <osegboifeoma3@gmail.com>
2025-12-04 12:47:37 +0100

    created a variable to store required paths.

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -12,12 +12,14 @@
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
+console.log(lastSlashIndex)
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
-
+const dir = filePath.slice(1, lastSlashIndex);
+const ext = filePath.slice(lastSlashIndex + 5);
+ console.log(dir)
+ console.log(ext)
 // https://www.google.com/search?q=slice+mdn
