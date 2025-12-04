f45be48f1e535a1d492aa8aaccdf9d2c5abb1a5b
Ifeoma <osegboifeoma3@gmail.com>
2025-12-04 12:53:23 +0100

    answered series of questions and how to rectify.

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,6 @@
 const cardNumber = 4533787178994213;
 const last4Digits = cardNumber.slice(-4);
+console.log(last4Digits)
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +8,15 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+//sol. 
+// I predict it will call the fourt digit from behind. this is because it wasn't specified to return from the last digit(3) to the fourth from behind(4). the correct code should be ...(-1, -4)
+
+// so i found out that ".slice" doesn't work on number except strings. to achieve this the code will be
+//const cardNumber = 4533787178994213;
+//const last4Digits = cardNumber.toString().slice(-4);
+//console.log(last4Digits)
+//adding .toString property mmakes the system convert the numbers to a strinngg before slicing.
+
+//OR
+// adding a "" to the numbers ("4533787178994213") to make it a string
