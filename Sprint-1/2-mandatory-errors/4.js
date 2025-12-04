50d903cb1d4becc53da159f2662fdd2d8e6902c5
Ifeoma <osegboifeoma3@gmail.com>
2025-12-04 12:54:23 +0100

    identified and suggested the error.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,6 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+//here there are no instructions but the proper arrangementt should be 
+// const 12HourClockTime = 08:53"
+// const 24hourClockTime = "20:53"
