1c025d1d7684deb92b89519d412581613de3e521
Carlos Marquina <carlosscripto@gmail.com>
2025-12-04 18:59:47 +0100

    Add explanation for the assignment operator in count.js

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+// the operator "=" is to asign a value or reasign 
+
+
