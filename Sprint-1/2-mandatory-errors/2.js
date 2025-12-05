0e2b5fc325a2b5833abd0758a1b12d546b901263
Ifeoma <osegboifeoma3@gmail.com>
2025-12-05 12:46:23 +0100

    Rearranged the lines in proper order.

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,7 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
 
 //this will give an error as the console is first called before the variable. the proper way is for the variable to be declared first before logging as the system starts to execute from the top line. 
