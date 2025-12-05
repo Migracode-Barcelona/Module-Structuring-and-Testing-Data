43219638ae0eade77c81ddbb84c3c94c825e66b7
OCC <occ@OCCs-MacBook-Pro.local>
2025-12-05 14:26:16 +0100

    Completed tasks in Sprint 1

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,9 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+console.log(age); 
+
+// We use 'let' to create a variable that can be reassigned.
+// 'age' starts at 33, then we add 1 to it and store the result back in 'age'.
+// console.log(age) outputs 34.
--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -20,3 +20,26 @@ console.log(`The percentage change is ${percentageChange}`);
 // d) Identify all the lines that are variable declarations
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+
+//Answer:
+// a) Function calls are on these lines:
+// carPrice = Number(carPrice.replaceAll(",", ""));
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
+// console.log(`The percentage change is ${percentageChange}`);
+
+// b) Error occurs on the line:
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+// Reason: missing comma between arguments of replaceAll. Fix: replaceAll(",", "")
+
+// c) Variable reassignment lines:
+// carPrice = Number(carPrice.replaceAll(",", ""));
+// priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
+
+// d) Variable declaration lines:
+// let carPrice = "10,000";
+// let priceAfterOneYear = "8,543";
+// const priceDifference = carPrice - priceAfterOneYear;
+// const percentageChange = (priceDifference / carPrice) * 100;
+
+// e) Number(carPrice.replaceAll(",", "")) removes all commas from the string and converts it to a number so calculations can be done.
