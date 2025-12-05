12fec1085d5156d0d5039b4978aa478c2f6092e0
Ifeoma <osegboifeoma3@gmail.com>
2025-12-05 12:53:40 +0100

    Renamed a variable for better representation.

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -1,15 +1,15 @@
 const movieLength = -733.77; // length of movie in seconds
 
 const remainingSeconds = movieLength % 60;
-const totalMinutes = (movieLength - remainingSeconds) / 60;
+const totalmovieHours = (movieLength - remainingSeconds) / 60;
 
-const remainingMinutes = totalMinutes % 60;
-const totalHours = (totalMinutes - remainingMinutes) / 60;
+const remainingMinutes = totalmovieHours % 60;
+const totalHours = (totalmovieHours - remainingMinutes) / 60;
 
 const result = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
 console.log(result);
 console.log(remainingSeconds)
-console.log(totalMinutes)
+console.log(totalmovieHours)
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
