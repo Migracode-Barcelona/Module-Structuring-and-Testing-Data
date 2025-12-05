e76affa517e76f859c031e2fb939d1868921a7af
Ifeoma <osegboifeoma3@gmail.com>
2025-12-05 12:44:45 +0100

    changed the keyword from "const" to "let"

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,6 +1,6 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
 
 //This will give an error because the variable "age" has already been assigned  by a "const" keyword. This means it cannot be changed.
