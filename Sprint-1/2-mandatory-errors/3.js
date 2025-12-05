744f59daf28ed18f55715be5beb29d2e73e0ce89
Ifeoma <osegboifeoma3@gmail.com>
2025-12-05 12:49:28 +0100

    Fxed error by commenting suggestions & adding("")

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,4 +1,4 @@
-const cardNumber = 4533787178994213;
+const cardNumber = "4533787178994213";
 const last4Digits = cardNumber.slice(-4);
 console.log(last4Digits)
 
@@ -10,13 +10,13 @@ console.log(last4Digits)
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
 
 //sol. 
-// I predict it will call the fourt digit from behind. this is because it wasn't specified to return from the last digit(3) to the fourth from behind(4). the correct code should be ...(-1, -4)
+// I predict it will call the fourth digit from behind. this is because it wasn't specified to return from the last digit(3) to the fourth from behind(4). the correct code should be ...(-1, -4)
 
 // so i found out that ".slice" doesn't work on number except strings. to achieve this the code will be
 //const cardNumber = 4533787178994213;
 //const last4Digits = cardNumber.toString().slice(-4);
 //console.log(last4Digits)
-//adding .toString property mmakes the system convert the numbers to a strinngg before slicing.
+//adding .toString property makes the system convert the numbers to a strinngg before slicing.
 
 //OR
 // adding a "" to the numbers ("4533787178994213") to make it a string
