5d5d67b936ebc6593621c8e6220457126e41f350
OCC-L0125 <joseph.yaw.ayitey@gmail.com>
2025-12-07 18:30:19 +0100

    implement extraction of dir part from file path

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,7 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
+const dir = filePath.slice(0, lastSlashIndex);
 const ext = ;
 
-// https://www.google.com/search?q=slice+mdn
+// https://www.google.com/search?q=slice+mdnss
