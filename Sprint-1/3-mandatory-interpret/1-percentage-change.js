54a9ff3d00b3619310d1fa6af45a2668783b5652
Ifeoma <osegboifeoma3@gmail.com>
2025-12-08 20:33:15 +0100

    added proper arrangements

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -12,14 +12,14 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
-//-- line 4, 5 and 10
+    //-- line 4, 5 and 10
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
-//-- line 4. there is a syntax error from ("," ""). the correctt way should innclude a comma tto separate the old and new to be changed(",", "")
+    //-- line 4. there is a syntax error from ("," ""). the correctt way should innclude a comma tto separate the old and new to be changed(",", "")
 
 // c) Identify all the lines that are variable reassignment statements -- line 4, 5.
 
 // d) Identify all the lines that are variable declarations. -- line 1, 2, 7, and 8.
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
-// it converts a string that contains commas into a number that can be used for calculations.
+    // it converts a string that contains commas into a number that can be used for calculations.
