e2d8142050f9419669c13e604c90c0c7d431c7a1
Carlos Marquina <carlosscripto@gmail.com>
2025-12-19 21:29:35 +0100

    Type Error was fixed

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,9 +1,17 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = cardNumber.toString().slice(-4);
+console.log(last4Digits)
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
 // Before running the code, make and explain a prediction about why the code won't work
+// I think it is not going to work because the method slice is not does not belong to the cardNumber variable
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// Why does it give this error? 
+// the method slice is not does not belong to the number data type
+
+// Is this what I predicted? 
+// yes
