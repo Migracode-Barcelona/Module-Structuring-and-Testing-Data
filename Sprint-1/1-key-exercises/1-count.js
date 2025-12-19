22e7be0bcb1772097a2e6956e94622a51d5aba97
Carlos Marquina <carlosscripto@gmail.com>
2025-12-19 15:40:14 +0100

    fix some words

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -5,6 +5,6 @@ count = count + 1;
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
 
-// the operator "=" is to asign a value or reasign 
+// the operator "=" is to assign a value or reassign 
 
 
