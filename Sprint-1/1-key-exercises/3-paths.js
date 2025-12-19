244d073d60865342ca3708b80d555a5d2084f575
Carlos Marquina <carlosscripto@gmail.com>
2025-12-19 21:00:04 +0100

    Reusable functions, and creation of the dir and ext variables

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,30 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const getExtension = (fileName) => {
+	const lastDotIndex = fileName.lastIndexOf(".");
+	const extension = fileName.slice(lastDotIndex);
+	return extension;
+};
+
+const getDirectory = (path) => {
+	const lastSlashIndex = path.lastIndexOf("/");
+	const pathBeforeLastSlash = path.slice(0, lastSlashIndex);
+	const lastIndexOf_PathBeforeLastSlash = pathBeforeLastSlash.lastIndexOf("/");
+	const dirname = pathBeforeLastSlash.slice(lastIndexOf_PathBeforeLastSlash + 1);
+	return dirname;
+};
+
+const ext = getExtension(base);
+const dir = getDirectory(filePath);
+
+console.log(`The directory part of ${filePath} is ${dir}`);
+console.log(`The extension part of ${filePath} is ${ext}`);
 
 // https://www.google.com/search?q=slice+mdn
+
+
+
+console.log(
+	getDirectory("sprint/exercise")
+)
