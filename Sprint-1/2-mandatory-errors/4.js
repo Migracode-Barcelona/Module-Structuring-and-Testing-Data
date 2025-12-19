614fadfb966895ef48700476a53307c0419d8caf
Carlos Marquina <carlosscripto@gmail.com>
2025-12-19 21:34:26 +0100

    names and time formats fixed

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,9 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+
+// Variables cannot begin with a number.
+// The variable names are reversed with respect to their format.
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+
+
+const hourClock12 = "08:53";
+const hourClock24 = "20:53";
